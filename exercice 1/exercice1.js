ea1385e29b9deaff27b95ae218ca7e92effc8204
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-20 16:26:11 +0100

    fini

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,13 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-if x > y
-    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+let x = 2;
+let y = 3;
 
+if (x > y) {
+    document.getElementById('monDiv').innerHTML = ("La variable x est supérieure à la variable y");
+
+}
+ else{
+     document.getElementById('monDiv').innerHTML =("la variable x nest âs superieure a la variable y");
+}
 
--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,15 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-if()
-{
+let x = 22;
+let y = 22;
 
+
+if(x === y) {
+    ++x;
+     ++x;
+     ++x;
+     alert("x est bien égal à y" + x);
 }
-
-{
-
+else {
+alert("x n'est pas égal à y " );
 }
